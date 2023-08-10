487394f9250474639e058f560117c62228899588
user <gushchin89@gmail.com>
2023-08-10 14:51:23 +0300

    start

---
+++ Communication.py
@@ -0,0 +1,28 @@
+from PyQt5 import QtCore, QtWidgets, QtSerialPort
+
+"""""
+class Com(QtSerialPort.QSerialPort):
+    def print(self):
+        print(1)
+
+"""""
+
+
+class Com():
+    serial = QtSerialPort.QSerialPort()
+    serial.setPortName("/dev/ttyACM0")
+    serial.setBaudRate(9600)
+
+    def open1(self):
+        try:
+            self.serial.open()
+            print(self.serial.isOpen())
+        except:
+            print(f"Cant opnet {self.serial.portName()}")
+
+    @classmethod
+    def read(self):
+        while self.serial.canReadLine():
+            text = self.serial.readLine().data().decode()
+            text = text.rstrip('\r\n')
+            return text
